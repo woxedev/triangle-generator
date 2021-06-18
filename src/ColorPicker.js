a83c3d7b997be6113794b2efdc75db31136fa6b7
WoxeOfficial <86092924+WoxeOfficial@users.noreply.github.com>
2021-06-18 15:22:46 +0300

    Remove unused vars

--- src/ColorPicker.js
+++ src/ColorPicker.js
@@ -1,4 +1,4 @@
-import React, {useState} from 'react';
+import React from 'react';
 import { TwitterPicker } from 'react-color';
 import {useDispatch, useSelector} from "react-redux";
 import {setColor} from "./redux/actions";
